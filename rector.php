11c6cacb3adcd2e97491a607e9b0902c3a13eae3
Tomas Norre Mikkelsen <tomas.mikkelsen@aoe.com>
2021-04-25 09:11:54 +0200

    [CI] Add Rector to GHA

---
+++ rector.php
@@ -0,0 +1,44 @@
+<?php
+
+declare(strict_types=1);
+
+use Rector\Core\Configuration\Option;
+use Rector\Php74\Rector\Property\TypedPropertyRector;
+use Rector\Set\ValueObject\SetList;
+use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigurator;
+
+return static function (ContainerConfigurator $containerConfigurator): void {
+    // get parameters
+    $parameters = $containerConfigurator->parameters();
+
+    $parameters->set(
+        Option::PATHS,
+        [
+            __DIR__ . '/app',
+        ]
+    );
+
+    $parameters->set(
+        Option::SKIP,
+        [
+            __DIR__ . '/app/Console/Kernel.php',
+            __DIR__ . '/app/Exceptions/',
+            __DIR__ . '/app/Http/Middleware/',
+            __DIR__ . '/app/Providers/',
+        ]
+    );
+
+    // Define what rule sets will be applied
+    $parameters->set(Option::SETS, [
+        SetList::DEAD_CODE,
+    ]);
+
+    $parameters->set(Option::AUTO_IMPORT_NAMES, false);
+
+    $parameters->set(
+        Option::AUTOLOAD_PATHS,
+        [
+            __DIR__ . '/app',
+        ]
+    );
+};
